00bcfb0228b25eeb92e0b2d270f446e60b7af280
Nistor Gheorghe <nistorgeorge404@gmail.com>
2022-05-16 16:37:11 +0300

    create 404 page

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const e = require('express');
 const express = require('express');
 
+// express app
 const app = express();
 
+// listen for requests
 app.listen(3000);
 
+// register view engine
 app.set('view engine', 'ejs');
 
+// middleware & static files
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -16,3 +20,7 @@ app.get('/', (req, res) => {
 app.get('/post', (req, res) => {
     res.render('post', {title: 'Post', cssFile: 'post.css'});
 });
+
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404', cssFile: '404.css'});
+})
